851319e46b1508ef752358d5d134eb8ea5d1d9c6
masprog2022 <masprog2022@gmail.com>
2022-07-28 22:57:05 +0100

    chore:add backend project

---
+++ backend/src/main/java/com/masprogtechs/appdoe/AppdoeApplication.java
@@ -0,0 +1,13 @@
+package com.masprogtechs.appdoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdoeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/masprogtechs/appdoe/AppdoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masprogtechs.appdoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
